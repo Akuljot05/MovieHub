c7da71aec100fcdf97240e5a8dfa6472b99f8599
Akuljot Singh <akuljot@gmail.com>
2019-05-27 16:47:12 +0530

    End to end domain modelling - rental

---
+++ MovieHub/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MovieHub.Dtos;
+
+namespace MovieHub.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalsDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
