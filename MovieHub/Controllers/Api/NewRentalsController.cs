97b7c4a2e62db95049f93277ec5012c598754e02
Akuljot Singh <akuljot@gmail.com>
2019-05-27 17:14:19 +0530

    added movie not available edge case to api

--- MovieHub/Controllers/Api/NewRentalsController.cs
+++ MovieHub/Controllers/Api/NewRentalsController.cs
@@ -28,6 +28,11 @@ namespace MovieHub.Controllers.Api
 
             foreach(var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
                 var rental = new Rental
                 {
                     Customer = customer,
